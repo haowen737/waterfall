e7de2e7bd277111f4fa822dd9606e09f4dbea019
Haowen737 <haowen737@gmail.com>
2020-11-11 20:58:27 +0800

    Using mocha

---
+++ packages/core/test/decorator.test.ts
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+
+import { Get, Controller, mapRoute } from '../src/lib/decorator'
+
+describe('should transfer http method', () => {
+
+  // @Controller('/hello')
+  // class HelloController {
+
+  //   @Get('/foo')
+  //   foo() {
+  //     return true
+  //   }
+
+  // }
+
+  // const route = mapRoute(new HelloController())
+  expect(true).to.be.true
+})
